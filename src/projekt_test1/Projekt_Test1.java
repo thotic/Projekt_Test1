ff5bbaa3c4f0ef121bdf6e73d2729251e638af95
thotic <thotic@gmx.de>
2015-06-16 11:03:04 +0200

    first test commit

---
+++ src/projekt_test1/Projekt_Test1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projekt_test1;
+
+/**
+ *
+ * @author thotic
+ */
+public class Projekt_Test1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Das ist ein GitHub Test");
+    }
+    
+}
