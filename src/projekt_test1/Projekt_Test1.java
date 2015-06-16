7d15610bf549bdd642cced226a6215c06ffaad59
thotic <thotic@gmx.de>
2015-06-16 11:25:54 +0200

    Lokale Änderung

--- src/projekt_test1/Projekt_Test1.java
+++ src/projekt_test1/Projekt_Test1.java
@@ -15,7 +15,7 @@ public class Projekt_Test1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Das ist ein GitHub Test");
+        System.out.println("Lokale Änderung");
         System.out.println("In GitHub geaendert!!!");
     }
     
