3b84385c3a1b46b78b16f5cbfc753ad8f83e41d5
thotic <thotic@gmx.de>
2015-06-16 11:08:00 +0200

    Update Projekt_Test1.java

--- src/projekt_test1/Projekt_Test1.java
+++ src/projekt_test1/Projekt_Test1.java
@@ -16,6 +16,7 @@ public class Projekt_Test1 {
      */
     public static void main(String[] args) {
         System.out.println("Das ist ein GitHub Test");
+        System.out.println("In GitHub geaendert!!!");
     }
     
 }
